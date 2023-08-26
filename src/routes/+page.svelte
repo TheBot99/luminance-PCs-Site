034f0ad4ab489489444a1ab1a2edd4800eca6e6e
TheBot99 <vinaymaryala@outlook.com>
2023-08-26 18:36:06 -0500

    First commit and changed title name on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Luminance PCs & tech</h1>
+<a href="/products">products</a>
