177d720bd60638fb76b71ca8575cf1281b23d935
TheBot99 <vinaymaryala@outlook.com>
2023-08-29 18:32:35 -0500

    Changed Nav Bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,14 @@
 <div class="container  flex justify-between items-center">
-    <h1 class = "text-left my-2 mx-2 text-gray-500 text-4xl"><a href="/">Luminance PCs & Tech</a></h1>
+    <h1 class = "text-left my-2 mx-2 text-gray-500 text-4xl font-semibold"><a href="/">Luminance PCs & Tech</a></h1>
     <ul class="text-gray-500 flex justify-center space-x-28 text-2xl mx-auto">
-        <li class="hover:text-white transition hover:underline duration-500 ease-in-out"><a href="/">Home</a></li>
-        <li class="hover:text-white transition hover:underline duration-500 ease-in-out"><a href="/listings/PCs">PCs</a></li>
-        <li class="hover:text-white transition hover:underline duration-500 ease-in-out"><a href="/listings/Peripherals">Peripherals</a></li>
+        <li class="hover:text-white transition hover:underline duration-500 ease-in-out hover:scale-105 font-semibold"><a href="/">Home</a></li>
+        <li class="hover:text-white transition hover:underline duration-500 ease-in-out hover:scale-105 font-semibold"><a href="/listings/PCs">PCs</a></li>
+        <li class="hover:text-white transition hover:underline duration-500 ease-in-out hover:scale-105 font-semibold"><a href="/listings/Peripherals">Peripherals</a></li>
     </ul>
-    <div class="flex justify-center items-center space-x-4">
-        <a href="/cart"><img src="/images/cart.png" alt="Cart" class="w-10 h-10"></a>
-    <div/>
+    
 </div>
 
-</div>
+
 
 
 
